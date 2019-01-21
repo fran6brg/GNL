559bc07f65159ffd9572195f68a6129bb5719306
Francis BERGER <fberger@e3r6p19.42.fr>
2019-01-21 16:26:36 +0100

    ok

--- valgrind/get_next_line.c
+++ valgrind/get_next_line.c
@@ -17,21 +17,21 @@ int					reach_nl_eof(int fd, char **str, char **buf)
 {
 	int				ret;
 	int				i;
-	char			*ptr;
+	char			*ptr; // pourquoi ??
 
 	i = 0;
 	while ((ret = read(fd, *buf, BUFF_SIZE)) > 0)
 	{
-		ptr = *buf;
-		ptr[ret] = '\0';
+		(*buf)[ret] = '\0';
 		ptr = *str;
 		if (*str)
 		 		*str = ft_strjoin(*str, *buf);
 		else
 		 		*str = ft_strdup(*buf);
-		//ft_memdel((void *)buf); // changement
 		if (i >= 1)
-				ft_memdel((void *)&ptr);
+		{
+				ft_strdel((void *)&ptr);
+		}
 		i++;
 		if (ft_strchr(*str, '\n'))
 		{
@@ -56,17 +56,15 @@ int					cp_str_in_line(char **line, char **str, int *ret1)
 		i++;
 	if (i == 0 || ptr[0] == '\0')
 	{
-		if (!(*line = ft_strdup("")))
+		if (!(*line = ft_strnew(0))) // auparavant = ft_strdup("")))
 				return (-1);
 	}
 	else if (!(*line = ft_strsub(ptr, 0, i)))
 		return (-1);
-	//if (ptr)
-	//			ft_memdel((void *)&ptr);
 	if (!ft_strlen(*str) && *ret1 == 0)
 		return (0);
 	if (i >= (int)ft_strlen(*str))
-			ft_strclr(*str); // ft_strclr 4000 -> 1600
+			ft_strclr(*str); // ne pas changer car bloque le multi
 	else
 		*str += (i + 1);
 	return (1);
